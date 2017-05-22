476b28c27a6547be703a941140885cd109fb8c86
oleh <oleh@devlop.loc>
2017-05-22 22:19:06 +0300

    cleaning empty values

--- app.js
+++ app.js
@@ -9,21 +9,23 @@ LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
 
 $scope.lunchItem = "";
-$scope.lunchItemSum = 0;
 $scope.message = "";
 $scope.messageClass = "";
 
 $scope.checkLunch = function() {
   var lunchItemsArray = $scope.lunchItem.split(',');
-  $scope.lunchItemSum = lunchItemsArray.length; // lenght = 1 - it's empty array!!! Becouse it is not lenght, it's last index+1
+  var cleanArray = lunchItemsArray.filter(function(item){return item;}) // cleaning empty values
+  var lunchItemSum = cleanArray.length; // lenght = 1 - it's maybe empty array!!! Becouse it is not lenght, it's last index+1
 
-  if($scope.lunchItemSum <= 1 && !lunchItemsArray[0]){
+  console.log(lunchItemSum);
+
+  if(lunchItemSum <= 1 && !lunchItemsArray[0]){
     $scope.message = "Please enter data first";
     $scope.messageClass = "text-danger";
-  }else if ($scope.lunchItemSum < 4) {
+  }else if (lunchItemSum < 4) {
     $scope.message = "Enjoy!";
     $scope.messageClass = "text-success";
-  }else if ($scope.lunchItemSum > 3) {
+  }else if (lunchItemSum > 3) {
     $scope.message = "Too much!";
     $scope.messageClass = "text-danger";
   }
